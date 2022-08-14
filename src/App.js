e85327c63a1c14b31813beb1047c5209105ae9cb
Anna Rudas <rudas.annamaria@gmail.com>
2022-08-14 17:30:19 +0200

    add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Credits from "./components/Credits";
 import Main from "./Main";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="wrapper">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/credits" element={<Credits />} />
